d465817be23b68b769d3f97c0960efe2bf61f57a
Kelsey Langlois and Megan Olson <kelsey@>
2017-10-24 08:55:49 -0700

    Add display for recipe.

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <h1>My First Angular 2 App</h1>
+    <div class="container">
+      <h1>Recipe Box</h1>
+      <ul>
+        <li *ngFor="let currentRecipe of recipes">
+          <h2>{{currentRecipe.title}}</h2>
+          <h3>Ingredients</h3>
+          <ul>
+            <li *ngFor="let currentIngredient of currentRecipe.ingredients">{{currentIngredient}}</li>
+          </ul>
+          <h3>Instructions</h3>
+          <p>{{currentRecipe.instructions}}</p>
+        </li>
+     </ul>
+    </div>
   `
 })
 
 export class AppComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      "Mac N Cheese",
+      ["Mac", "Cheese"],
+      "Add Cheese to Mac. Heat and Serve."
+    )
+  ]
+}
 
+export class Recipe {
+  constructor(public title: string, public ingredients: string[], public instructions: string) {}
 }
